a4dbb70e2ede0ae4f47aceac8af855cb6f54d3f7
Tanish <tanish.bengani@gmail.com>
2021-10-10 00:08:50 +0800

    finished home banner

--- app/static/main.js
+++ app/static/main.js
@@ -44,3 +44,45 @@ if (theme) {
           e.classList.add("important-nav");
        }
     });
+
+  //DEFINE VARIABLE
+var balls = document.getElementsByClassName("ball");
+
+//event.clientX FOR GET THE HORIZONTAL COORDINATE OF THE MOUSE
+//event.clientY FOR GET THE VERTICAL COORDINATE OF THE MOUSE
+//window.innerWidth FOR GET THE BROWSER WIDTH
+//window.innerHeight FOR GET THE BROWSER HEIGHT
+
+document.onmousemove = function(){
+  var x = event.clientX * 100 / window.innerWidth + "%";
+  var y = event.clientY * 100 / window.innerHeight + "%";
+
+  for(var i=0;i<2;i++){
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = "translate(-"+x+",-"+y+")";
+  }
+}
+
+var balls = document.getElementsByClassName("ball");
+ 
+//event.clientX FOR GET THE HORIZONTAL COORDINATE OF THE MOUSE
+//event.clientY FOR GET THE VERTICAL COORDINATE OF THE MOUSE
+//window.innerWidth FOR GET THE BROWSER WIDTH
+//window.innerHeight FOR GET THE BROWSER HEIGHT
+ 
+var eyeball = document.querySelectorAll('.ball')
+window.addEventListener('mousemove', function(e) {
+  var screenSizeX = this.innerWidth
+  var screenSizeY = this.innerHeight
+  var cursorX = e.pageX;
+  var cursorY = e.pageY;
+  var percentX = (cursorX/screenSizeX) * 100
+  var percentY = (cursorY/screenSizeY) * 100
+
+  eyeball.forEach(function(eyeball) {
+    eyeball.style.top = percentY + '%';
+    eyeball.style.left = percentX + '%';
+
+  })
+})
